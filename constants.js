14ed4ea197707e5a029e5f4ff01e8d7857763b30
cutlerwater <cutlerwater2@live.com>
2021-09-27 09:50:18 -0400

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,57 @@
+export const projects = [
+  {
+    title: 'Student Study Portal',
+    description:"Building this project now.",
+    image: '/images/todo.jpg',
+    tags: ['Django', 'Heroku', 'Python', 'HTML', 'CSS'],
+    source: 'https://github.com/cutlerwater/student_portal',
+    visit: 'https://cw-student-portal.herokuapp.com/',
+    id: 0,
+  },
+  {
+    title: 'Dentist Website',
+    description: "Full Dental website for making appointments using Django, HTML, JavaScript and CSS. ",
+      image: '/images/dental-care.png',
+      tags: ['Django', 'Python', 'HTML', 'CSS'],
+    source: 'https://github.com/cutlerwater/DentistWebsite',
+    visit: 'https://gcutler.herokuapp.com/',
+    id: 1,
+  },
+  {
+    title: 'Covid Tracker',
+    description: "This is a Covid Tracking app I've built using React.JS.",
+    image: '/images/covid.png',
+    tags: ['React', 'JavaScript', 'HTML'],
+    source: 'https://github.com/cutlerwater/CovidTracker',
+    visit: 'https://cutlerwater-covid-tracker.netlify.app/',
+    id: 2,
+  },
+  {
+    title: 'Piano App',
+    description: "This is a code repository for my simple piano app.",
+      image: '/images/piano.png',
+      tags: ['JavaScript', 'CSS', 'HTML'],
+    source: 'https://github.com/cutlerwater/SimplePianoApp',
+    visit: 'https://cutlerwateritblog.com/WebProjects/Piano/index.html',
+    id: 3,
+  },
+  {
+    title: 'CSS Apps',
+    description: "This is a collection of small CSS projects ",
+      image: '/images/piano.png',
+      tags: ['JavaScript', 'CSS', 'HTML'],
+    source: 'https://github.com/cutlerwater/CSS-in-28-days',
+    visit: 'https://cutlerwateritblog.com/WebProjects/CSSProjects/29daysofCSS/index.html',
+    id: 3,
+  }
+];
+
+export const TimeLineData = [
+  { year: 2001, text: 'Started my IT journey with my A+ certification', },
+  { year: 2002, text: 'Worked as an Installation Tech for the United States Senate', },
+  { year: 2005, text: 'Worked as a systems and network engineer for the Social Security Administration', },
+  { year: 2010, text: 'Started my SharePoint and web development journey', },
+  { year: 2019, text: 'Dove head first into the Power Platform', },
+  { year: 2020, text: 'Covid hits and I blast forward', },
+  { year: 2021, text: 'Started my own platform', },
+];
