a7610f0a26c1e28f92cf0158aa035ac35444ffa9
cutlerwater <cutlerwater2@live.com>
2021-12-01 17:43:42 -0500

    Add files via upload

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -17,8 +17,8 @@ const Hero = (props) => (
         put my skills into some kind of action.
       </SectionText>
       <SectionText>
-        Some other ideas I have to try to implement are a search engine (coming soon), a music player, more 3d fun stuff, more CSS work, an E-commerce (tried this several times already) site, an updated news site
-      , an Instagram type clone, a netflix clone, some youtube videos, an e-learning site, etc...
+        Some other ideas I have to try to implement are a music player, more 3d fun stuff, more CSS work, an E-commerce (tried this several times already) site, an updated news site,
+         an Instagram type clone, a netflix clone, some youtube videos, an e-learning site, etc...
       </SectionText>
       <SectionText>
         If you have any ideas, suggestions, comments, questions, please let me know at cutlerwater2@live.com and please, if you would, go to my GitHub site at https://github.com/cutlerwater and star some of my 
